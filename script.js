c3222c55da714392f541f5f78b3b23489cbe5987
MrMoyii <omarmoyano7w@gmail.com>
2022-05-02 13:24:46 -0300

    agregado de defer en el scrip del html. agreado de la accion del boton y creacion de otra funcion

--- script.js
+++ script.js
@@ -1,36 +1,74 @@
-let valorDinero = document.getElementById('txtMontoAInvertir').value;
-let cantidadDias = document.getElementById('txtCantidadDias').value;
+document.getElementById('btnCalcularMonto').addEventListener('click', (e)=> {
+    e.preventDefault();
+    
+    //agarramos los valores de los inputs
+    let valorDinero = document.getElementById('txtMontoAInvertir').value;
+    let cantidadDias = document.getElementById('txtCantidadDias').value;
 
-function validarMontoAInvertir(valorDinero){
-    //valida si el monto a invertir ingreasdo es un número o si es menor a 1000
-    if(isNaN(valorDinero) || valorDinero < 1000) {
-        return false;
+    ordenLlamadoFunciones();
+
+
+
+
+
+
+    //-----------Funciones-----------//
+
+
+    function ordenLlamadoFunciones(){
+
+        /*
+
+        aqui irian las validaciones de nombre y apellido 
+        
+        */
+
+        //si devuelve false es porque no cumple con los requisitos, entonces uso ! para que entre al if,
+        //en caso contrario no ingresaria y no se mostraria el error
+        if (!validarMontoAInvertir(valorDinero)){
+            //mensaje de error//
+        };
+        if(!validarDiasAInvertir(cantidadDias)){
+            //mensaje de error//
+        }
     }
-    return true;
-}
 
-function validarDiasAInvertir(cantidadDias){
-    //valida si los días es un número y como minimo 30
-    if(isNaN(cantidadDias) || cantidadDias <= 30) {
-        return false;
+    
+    function validarMontoAInvertir(valorDinero){
+        //valida si el monto a invertir ingreasdo es un número o si es menor a 1000
+        if(isNaN(valorDinero) || valorDinero < 1000) {
+            return false;
+        }
+        return true;
     }
-    return true;
-}
-
-function montoFinal(valorDinero, cantidadDias){
-    let porcentaje;
-    //validacion de días para el calculo del porcentaje
-    if (cantidadDias >= 30 && cantidadDias <= 60){
-        porcentaje = 40;
-    }else if(cantidadDias > 60 && cantidadDias <= 120){
-        porcentaje = 45;
-    }else if(cantidadDias > 120 && cantidadDias <= 360){
-        porcentaje = 50;
-    }else{
-        porcentaje = 65;
+    
+    function validarDiasAInvertir(cantidadDias){
+        //valida si los días es un número y como minimo 30
+        if(isNaN(cantidadDias) || cantidadDias <= 30) {
+            return false;
+        }
+        return true;
     }
-    //formula
-    let montoFinal =  valorDinero + (valorDinero * (cantidadDias/360) * (porcentaje/100));
+    
+    function montoFinal(valorDinero, cantidadDias){
+        let porcentaje;
+        //validacion de días para el calculo del porcentaje
+        if (cantidadDias >= 30 && cantidadDias <= 60){
+            porcentaje = 40;
+        }else if(cantidadDias > 60 && cantidadDias <= 120){
+            porcentaje = 45;
+        }else if(cantidadDias > 120 && cantidadDias <= 360){
+            porcentaje = 50;
+        }else{
+            porcentaje = 65;
+        }
+        //formula
+        let montoFinal =  valorDinero + (valorDinero * (cantidadDias/360) * (porcentaje/100));
+    
+        return montoFinal;
+    }
 
-    return montoFinal;
-}
+});
+
+
+
