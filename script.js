b0baf2a07e62cbc39fba5dab4b1c47aef06bc40a
MrMoyii <omarmoyano7w@gmail.com>
2022-05-02 12:51:05 -0300

    cambio del nombre de variable y creacion de la funcion del monto final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let valorDinero = document.getElementById('txtMontoAInvertir').value;
-let valorDias = document.getElementById('txtCantidadDias').value;
+let cantidadDias = document.getElementById('txtCantidadDias').value;
 
 function validarMontoAInvertir(valorDinero){
     //valida si el monto a invertir ingreasdo es un número o si es menor a 1000
@@ -9,14 +9,28 @@ function validarMontoAInvertir(valorDinero){
     return true;
 }
 
-function validarDiasAInvertir(valorDias){
+function validarDiasAInvertir(cantidadDias){
     //valida si los días es un número y como minimo 30
-    if(isNaN(valorDias) || valorDias <= 30) {
+    if(isNaN(cantidadDias) || cantidadDias <= 30) {
         return false;
     }
     return true;
 }
 
-function montoFinal(valorDinero, valorDias){
-    
+function montoFinal(valorDinero, cantidadDias){
+    let porcentaje;
+    //validacion de días para el calculo del porcentaje
+    if (cantidadDias >= 30 && cantidadDias <= 60){
+        porcentaje = 40;
+    }else if(cantidadDias > 60 && cantidadDias <= 120){
+        porcentaje = 45;
+    }else if(cantidadDias > 120 && cantidadDias <= 360){
+        porcentaje = 50;
+    }else{
+        porcentaje = 65;
+    }
+    //formula
+    let montoFinal =  valorDinero + (valorDinero * (cantidadDias/360) * (porcentaje/100));
+
+    return montoFinal;
 }
