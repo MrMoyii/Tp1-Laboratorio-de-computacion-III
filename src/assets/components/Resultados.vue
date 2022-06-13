b6d7b72061d1a5a11e25ec19e9a5a779067d8b7f
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-06-12 21:35:58 -0300

    Remover estilos repetidos en componente Resultados
    Los estilos eliminados ya se encuentran definidos en style.css,
    pertenecen a un elemento fuera del componente Resultados.

--- src/assets/components/Resultados.vue
+++ src/assets/components/Resultados.vue
@@ -47,13 +47,6 @@ export default {
 </template>
 
 <style>
-#resultados-container {
-    height: 55%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-}
-
 #resultados-container h2 {
     text-align: center;
     font-size: 2rem;
