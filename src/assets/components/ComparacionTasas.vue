7257a33a5d58184e6570b4b3f8474a310aa5904b
Lisandro Bonino <lisandrobonino4@protonmail.com>
2022-06-12 21:31:15 -0300

    Agregar tabla comparativa de tasas como componente de vue

---
+++ src/assets/components/ComparacionTasas.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <table class="tablaTasas">
+        <caption class="tituloCuadro">Cuadro comparativo tasas y días</caption>
+
+        <tr>
+            <th>Cantidad de días</th>
+            <th>Porcentaje %</th>
+        </tr>
+        <tr>
+            <td>30 - 60</td>
+            <td>40</td>
+        </tr>
+        <tr>
+            <td>61 - 120</td>
+            <td>45</td>
+        </tr>
+        <tr>
+            <td>121 - 360</td>
+            <td>50</td>
+        </tr>
+        <tr>
+            <td>361 en adelante</td>
+            <td>65</td>
+        </tr>
+    </table>
+</div>
+</template>
+
+<style>
+.tituloCuadro {
+    margin-top: 5%;
+    margin-bottom: 5px;
+    text-decoration: underline;
+}
+
+.tablaTasas {
+    padding: 5px;
+    margin: 0 auto;
+}
+</style>
